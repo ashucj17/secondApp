96f103bb8654e393b5caf9edeb9e9410d1f43e03
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-04-09 21:37:31 +0530

    class based component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom/client"
 
+// Functional component
+// const App = () => {
+//     return (
+//         <div> My app </div>
+//     )
+// }
 
-const App = () => {
-    return (
-        <div> My app </div>
-    )
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+class App extends Component{
+    render(){
+        return(
+            <h1>Class based component</h1>
+        )
+    }
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// const container = document.getElementById('root')
+// const root = ReactDOM.createRoot(container)
+// root.render(<App/>)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
